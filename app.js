836265e64e5a68ad29945e0b8bf23c4d844a4d31
Casey Ward <hitchkra@hotmail.com>
2023-03-30 11:00:12 -0400

    JS HOF complete

--- app.js
+++ app.js
@@ -1,4 +1,46 @@
-console.log("Hello World!\n==========\n");
+// Exercise 1
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+function plus(num) {
+    return function (plusNumber) {
+        return num + plusNumber;
+    };
+}
+
+const plus2 = plus(2);
+    
+console.log(plus2(2)); 
+console.log(plus2(5));
+
+const plus100 = plus(100);
+
+console.log(plus100(100));
+console.log(plus100(5));
+
+
+let users = [
+    {
+      name: "Frodo",
+      age: 50,
+      score: 85,
+      isActive: false,
+    },
+    {
+      name: "Sam",
+      age: 38,
+      score: 94,
+      isActive: true,
+    },
+    {
+      name: "Merry",
+      age: 36,
+      score: 82,
+      isActive: true,
+    },
+    {
+      name: "Pippin",
+      age: 26,
+      score: 77,
+      isActive: false,
+    },
+  ];
+// Exercise 2 ForEach
